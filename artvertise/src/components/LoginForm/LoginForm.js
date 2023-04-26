b99baf9be1b5fc78d6a6fb8c94427a82b3348f87
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-04-26 09:21:09 +0300

    Login Authentication

--- artvertise/src/components/LoginForm/LoginForm.js
+++ artvertise/src/components/LoginForm/LoginForm.js
@@ -1,42 +1,50 @@
-import React, { useState } from 'react'
-import './LoginFormStyles.css'
-import AuthService from '../../Services/auth.service.js';
-import StorageService from '../../utils/storage.utils.js';
-import { Link, useHistory } from 'react-router-dom'
-// import image0 from '../images/image-7.jpg'
-
+import React, { useState } from 'react';
+import './LoginFormStyles.css';
+import { Link, useHistory } from 'react-router-dom';
+import AuthService from '../../Services/auth.service'
+import StorageService from '../../utils/storage.utils';
 
 function LoginForm() {
-    const history = useHistory();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  const history = useHistory()
 
-     const handleSubmit = async (event) => {
-	 event.preventDefault();
-    try {
-      const response = await AuthService.login(email, password);
-	if (response.status === 200)
-	{
-	    const data = await response.json();
-	    StorageService.saveUser(data.user);
-	    history.push('/artists');
-	} else {
-	    const data = await response.json()
-	    alert(data.error);
+  console.log(email);
+  console.log(password);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+	try {
+		const response = await AuthService.login(email, password);
+		console.log(response)
+		console.log('USER>>>', response.user)
+		console.log('USER JSON ----->', response.success)
+
+		if(response.success === 'ok') {
+			const data = await response.user;
+			console.log(data)
+			StorageService.saveUser(data)
+			console.log("about to redirect to /artists");
+
+			history.push('/artists')
+
+			// window.location.href = '/artists';
+		} else {
+			// const data = await response.user
+	    	// alert(data.error);
+			alert("Cannot login")
+		} 
+	} catch (error) {
+		console.error(error)
 	}
-    } catch (error) {
-	console.error(error);
-    }
   };
 
   return (
     <div className='login-form'>
       <div className='left'>
         <Link to='/' className='nav-link'>
-          <h1 className='logo-login'>
-            Artvertise <i className='fa-brands fa-artstation'></i>{' '}
-          </h1>
+          <h1 className="logo-login">Artvertise <i className="fa-brands fa-artstation"></i> </h1>
         </Link>
         <form className='login-forminput' onSubmit={handleSubmit}>
           <input
@@ -44,29 +52,21 @@ function LoginForm() {
             type='text'
             required
             value={email}
-            onChange={(event) => setEmail(event.target.value)}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             placeholder='Password*'
             type='password'
             required
             value={password}
-            onChange={(event) => setPassword(event.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <button className='login-button' type='submit'>
-            Sign in
-          </button>
+          <button className='login-button'>Sign in</button>
         </form>
-        <p className='signup-redirect'>
-          Don't have an account yet? <a href='/signup'>Sign up</a>
-        </p>
+        <p className='signup-redirect'>Don't have an account yet? <a href='/signup'>Sign up</a></p>
       </div>
       <div className='right'>
-        <img
-          className='login-image'
-          src='https://images.pexels.com/photos/4348403/pexels-photo-4348403.jpeg?auto=compress&cs=tinysrgb&w=400'
-          alt='login'
-        />
+        <img className='login-image' src='https://images.pexels.com/photos/4348403/pexels-photo-4348403.jpeg?auto=compress&cs=tinysrgb&w=400' alt='login'/>
       </div>
     </div>
   );
