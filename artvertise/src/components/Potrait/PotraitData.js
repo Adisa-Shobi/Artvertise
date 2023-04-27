daaddbfdb879c0de0f95986e072e3b6b0be16b4a
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-04-27 23:18:50 +0300

    Frontend Update

--- artvertise/src/components/Potrait/PotraitData.js
+++ artvertise/src/components/Potrait/PotraitData.js
@@ -6,7 +6,7 @@ function PotraitData(props) {
   return (
     <div className='p-card'>
         <div className='p-image'>
-            <img src={props.image} alt="artimage"/>
+            <img className='potrait-image'src={props.image} alt="artimage"/>
         </div>
         <h4>{props.heading}</h4>
         <p>{props.text}</p>
