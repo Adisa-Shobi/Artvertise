ab2967563a51df7bc1d1f77a7bc7ebe74739fd0a
Kwame8919 <101393080+Kwame8919@users.noreply.github.com>
2023-04-27 09:36:22 +0000

    payment route

---
+++ backend/app/routes/paymentStripe.js
@@ -0,0 +1,19 @@
+const Stripe = require('stripe')(process.env.SECRET_KEY);
+
+app.post('/payment', async (req, res) => {
+  let status, error;
+  const { token, amount } = req.body;
+  console.log(token);
+  try {
+    await Stripe.charges.create({
+      source: token.id,
+      amount,
+      currency: 'usd',
+    });
+    status = 'success';
+  } catch (error) {
+    console.log(error);
+    status = 'Failure';
+  }
+  res.json({ error, status });
+});
