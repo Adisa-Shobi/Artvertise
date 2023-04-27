e58862d5fd5d7fa48d947c4984a7b87e433c7ea0
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-04-27 17:15:32 +0300

    Payment Integration with Stripe

--- artvertise/src/components/PaymentComponent/PaymentComp.js
+++ artvertise/src/components/PaymentComponent/PaymentComp.js
@@ -3,15 +3,68 @@ import './PaymentCompStyles.css'
 import NavbarB from '../NavbarBasket/NavbarB'
 import CheckoutProduct from '../CheckoutProducts/CheckoutProduct'
 import { useStateValue } from '../StateProvider';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import StorageService from '../../utils/storage.utils';
+import { getBasketTotal } from '../reducer';
+
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
 
 function PaymentComp() {
   // eslint-disable-next-line
   const [{basket}, dispatch] = useStateValue();
+  const history = useHistory();
 
   const user = StorageService.getUser();
   console.log(user);
+
+  const publishableKey = "pk_test_51MybTvKnRw0cJpMd1Rn07OI8LytvfrclFT10VIlOtGjOEeC1NDGsAnX0PQEzUp71XA4HV1314zbq9XTlwm6qIa6x007lkFlbIP";
+
+  const handleSuccess = () => {
+    MySwal.fire({
+        icon: "success",
+        title: "Payment was Sucessful",
+        time: 4000,
+    });
+  };
+
+  const handleFailure = () => {
+    MySwal.fire({
+        icon: "failure",
+        title: "Payment was not Sucessful",
+        time: 4000,
+    });
+  };
+
+  const payNow = async (token) => {
+    try {
+        const response = await axios({
+            url: "http://localhost:5001/payment",
+            method: "post",
+            data: {
+                amount: `${getBasketTotal(basket) * 100}`,
+                token,
+            },
+        });
+        if (response.status === 200) {
+            handleSuccess();
+
+            dispatch({
+                type: 'EMPTY_BASKET'
+            });
+
+            history.replace('/artists')
+        }
+    } catch (error) {
+        handleFailure();
+    }
+  }
+
+  
 
 
   return (
@@ -46,6 +99,7 @@ function PaymentComp() {
                         image={item.image}
                         price={item.price}
                         rating={item.rating}
+                        hideButton
                         />
                     ))}
                 </div>
@@ -55,6 +109,14 @@ function PaymentComp() {
         <div className='payment__section'>
                 <div className='payment__title'>
                     <h3>Payment Method</h3>
+                    <StripeCheckout
+                        stripeKey={publishableKey}
+                        label="Pay Now"
+                        name="Pay With Credit Card"
+                        amount={getBasketTotal(basket) * 100}
+                        description={`Your total is $${getBasketTotal(basket)}`}
+                        token={payNow}
+                    />
                 </div>
         </div>
     </div>
