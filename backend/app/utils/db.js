41dc7a9ef64b782a28fbe2393530ca93294a897e
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-07-20 23:09:15 +0000

    Update: database uri support changed

--- backend/app/utils/db.js
+++ backend/app/utils/db.js
@@ -3,7 +3,7 @@ const { MongoClient } = require('mongodb');
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || '27017';
 const DB_NAME = process.env.DB_NAME || 'db_artvertise';
-const uri = `mongodb://${DB_HOST}:${DB_PORT}`;
+const uri = process.env.DB_URI || `mongodb://${DB_HOST}:${DB_PORT}`;
 
 class DBClient {
   /**
