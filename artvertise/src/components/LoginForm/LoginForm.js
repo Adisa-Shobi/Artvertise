ba97251c01f6469ed61253340edab48b18f7d264
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-07-21 18:49:09 +0000

    Update: fixed

--- artvertise/src/components/LoginForm/LoginForm.js
+++ artvertise/src/components/LoginForm/LoginForm.js
@@ -22,7 +22,7 @@ function LoginForm() {
 		//console.log('USER JSON ----->', response.success)
 
 		if(response.ok) {
-		    const data = await response.json.data;
+		    const data = await response.json();
 			console.log(data)
 			StorageService.saveUser(data)
 			console.log("about to redirect to /artists");
